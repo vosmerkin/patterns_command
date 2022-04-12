96da83df275b5bef315edd6ab0fe33c200b1f297
Evgen <vosmerkin@mail.ru>
2022-04-12 09:29:03 +0300

    remoteControlTest

---
+++ src/main/java/remoteControlTest.java
@@ -0,0 +1,14 @@
+import commands.LightOnCommand;
+import commands.SimpleRemoteControl;
+import devices.Light;
+
+public class remoteControlTest {
+    public static void main(String[] args) {
+        SimpleRemoteControl remote = new SimpleRemoteControl();
+        Light light = new Light();
+        LightOnCommand lightOn = new LightOnCommand(light);
+
+        remote.setCommand(lightOn);
+        remote.buttonWasPressed();
+    }
+}
